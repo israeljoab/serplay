cefcec3b1cfbfd68b2d64eba5b7377a6f950e7f3
Israel Joab <israeljoab@gmail.com>
2020-05-16 19:05:33 -0300

    Atualizações e correções

--- registro.php
+++ registro.php
@@ -35,14 +35,20 @@
                     <h2>Login</h2>
                     <p>
                         <?php echo $conta->getErro(Constantes::$loginFalhou); ?>
+                        <label for="loginMatricula">👨‍🎓 Matrícula</label>
                         <input id="loginMatricula" name="loginMatricula" type="text" placeholder="Digite sua matrícula" required>
                     </p>
                     
                     <p>
-                        <input id="loginSenha" name="loginSenha" type="password" placeholder="Senha" required>
+                        <label for="loginMatricula">🔑 Digite sua Senha</label>
+                        <input id="loginSenha" name="loginSenha" type="password" placeholder="Digite sua senha" required>
                     </p>
 
                     <button type="submit" name="botaoLogin">Entrar</button>
+
+                    <div class="criarContaTexto">
+                        <span id="ocultarLogin">Não tem cadastro? Faça seu cadastro agora mesmo!🚩</span>
+                    </div>
                     
                 </form>
 
@@ -51,11 +57,13 @@
                     <p>
                         <?php echo $conta->getErro(Constantes::$matriculaTamanho); ?>
                         <?php echo $conta->getErro(Constantes::$matriculaExistente); ?>
+                        <label for="username">👨‍🎓👩‍🎓 Matrícula</label>
                         <input id="matricula" name="matricula" type="text" placeholder="Digite sua matrícula" value="<?php getEntradaDados('matricula') ?>" required>
                     </p>
 
                     <p>
                         <?php echo $conta->getErro(Constantes::$nomeCompleto); ?>
+                        <label for="username">💬 Nome Completo</label>
                         <input id="nomeCompleto" name="nomeCompleto" type="text" placeholder="Digite seu nome" value="<?php getEntradaDados('nomeCompleto') ?>" required>
                     </p>
 
@@ -63,10 +71,12 @@
                         <?php echo $conta->getErro(Constantes::$emailsDiferentes); ?>
                         <?php echo $conta->getErro(Constantes::$emailInvalido); ?>
                         <?php echo $conta->getErro(Constantes::$emailExistente); ?>
+                        <label for="username">📧 Seu email</label>
                         <input id="email" name="email" type="email" placeholder="exemplo@exemplo.com" value="<?php getEntradaDados('email') ?>" required>
                     </p>
 
                     <p>
+                        <label for="username">📩 Confirmar email</label>
                         <input id="emailConfirmacao" name="emailConfirmacao" type="email" placeholder="Confirmar E-mail" value="<?php getEntradaDados('emailConfirmacao') ?>" required>
                     </p>
 
@@ -74,14 +84,20 @@
                         <?php echo $conta->getErro(Constantes::$senhasDiferentes); ?>
                         <?php echo $conta->getErro(Constantes::$senhasAlfanumericas); ?>
                         <?php echo $conta->getErro(Constantes::$senhasTamanho); ?>
+                        <label for="username">🔐 Digite sua senha</label>
                         <input id="senha" name="senha" type="password" placeholder="Senha" required>
                     </p>
 
                     <p>
+                    <label for="username">🔏 Confirme sua senha</label>
                         <input id="senhaConfirmacao" name="senhaConfirmacao" type="password" placeholder="Confirme sua Senha" required>
                     </p>
 
                     <button type="submit" name="botaoRegistar">INSCREVER-SE</button>
+
+                    <div class="criarContaTexto">
+                        <span id="ocultarRegistro">Já tem uma conta? Fazer login.🚪</span>
+                    </div>
                     
                 </form>
             </div>

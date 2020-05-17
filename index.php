b272251edb9a8da998f1a6d777d38a248ecb4634
Israel Joab <israeljoab@gmail.com>
2020-05-17 16:23:40 -0300

    Atualizações

--- index.php
+++ index.php
@@ -14,15 +14,38 @@ else {
 
 <html>
 <head>
-    <title>Bem vindo ao SerPlay!</title>
+	<title>Bem Vindo ao SerPlay!</title>
 
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
-    <div id="playBarConteiner">
+	
 
-    </div>
+	<div id="nowPlayingBarContainer">
+
+		<div id="nowPlayingBar">
+
+			<div id="nowPlayingLeft">
+
+			</div>
+
+			<div id="nowPlayingCenter">
+
+			</div>
+
+			<div id="nowPlayingRight">
+
+			</div>
+
+
+
+
+		</div>
+
+	</div>
+
+
 </body>
 
 </html>

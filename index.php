b336e55951efd77fa7ba325692ca30e5eb5361dc
Israel Joab <israeljoab@gmail.com>
2020-05-18 13:39:34 -0300

    Atualizações

--- index.php
+++ index.php
@@ -25,6 +25,41 @@ if (isset($_SESSION['usuarioLogado'])) {
 
         <div id="topContainer">
 
+            <div id="navBarContainer">
+                <nav class="navBar">
+
+                    <a href="index.php" class="logo">
+                        <img src="assets/imagens/icons/logo.png">
+                    </a>
+
+                    <div class="grupo">
+
+                        <div class="navItem">
+                            <a href="pesquisar.php" class="navItemLink">Pesquisar
+                                <img src="assets/imagens/icons/search.png" class="icon" alt="Pesquisar">
+                            </a>
+                        </div>
+
+                    </div>
+
+                    <div class="grupo">
+                        <div class="navItem">
+                                <a href="categorias.php" class="navItemLink">Categorias</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="seucurso.php" class="navItemLink">Seu Curso</a>
+                        </div>
+
+                        <div class="navItem">
+                            <a href="perfil.php" class="navItemLink">Perfil</a>
+                        </div>
+                    </div>
+    
+
+                </nav>
+            </div>
+
         </div>
 
         <div id="nowPlayingBarContainer">

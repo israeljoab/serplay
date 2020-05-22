1b1b6fa09fadb53006a7580946ada0a92224c31e
Israel Joab <israeljoab@gmail.com>
2020-05-22 08:56:17 -0300

    Atualizações

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<?php
+include("includes/config.php");
+
+//session_destroy();
+
+if (isset($_SESSION['usuarioLogado'])) {
+    $usuarioLogado = $_SESSION['usuarioLogado'];
+} else {
+    header("Location: registro.php");
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Bem Vindo ao SerPlay!</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <div id="mainContainer">
+
+        <div id="topContainer">
+
+            <?php include("includes/navBarContainer.php");?>
+
+            <div id="mainViewContainer">
+
+                <div id="mainContent">

06bc0f27c0f0f05b86b25193bbdee8d124731b6c
Israel Joab <israeljoab@gmail.com>
2020-05-24 16:49:41 -0300

    atualizações

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
 <?php include("includes/header.php"); ?>
 
+<h1 class="textoTopGrande">Podcasts SerPlay</h1>
+
+<div class="gridViewContainer">
+
+    <?php
+      $cursoQuery = mysqli_query($conex, "SELECT * FROM cursos ORDER BY RAND() LIMIT 10");
+      while($row = mysqli_fetch_array($cursoQuery)) {
+
+           
+          
+        echo "<div class='gridViewItem'>
+                <a href='curso.php?id=" . $row['id'] . "'>
+                  <img src='" . $row['caminhoCapa'] . "'>
+
+                  <div class='gridViewInfo'>"
+                      . $row['titulo'] .
+                  "</div>
+                </a>
+        
+            </div>";
+
+
+
+      }
+
+    ?>
+
+</div>
+
 <?php include("includes/footer.php"); ?>
